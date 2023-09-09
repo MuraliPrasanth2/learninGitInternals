27b351aa9246d273eac55cdd288bb1ccadd3b66b
Murali Prasanth <mprasanth18@gmail.com>
2023-09-09 15:42:21 +0530

    Inital commit.

---
+++ app.js
@@ -0,0 +1,6 @@
+// this is a line added to see if git creates a different hash for the same file if the file contents changes.
+function main() {
+	console.log("This is  a sample file that we will be adding inside the git for checking how git stores this file in the .git/objects directory");
+}
+
+main();
